2d4730cdcd9d344cc5c26622f4dbaeb130e45e06
synzex <markzaharov2007@gmail.com>
2026-09-11 15:37:29 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Project-under-control/',
   server: {
     proxy: {
       '/api': {
